e49eb43b67953a023d7f1896d025efaa3823f2a5
Sarthak Shrivastava <bitfumes@gmail.com>
2017-09-04 12:47:42 +0530

    Intro to jsx

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+function cartoon(name='Pikachu',show='Pokemon'){
+  return <div>
+            <h1 className='someclass'>cartoon name is {name} and its show is {show}</h1>
+            <h2>Hello Bitfumes</h2>
+          </div>
+}
+
 ReactDOM.render(
-  <h1>Welcome to Bitfumes</h1>,
+  cartoon('Jasmine','Alladin'),
   document.getElementById('root')
 );
